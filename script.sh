7abf99887d8e0eae7752b4cd75e4a97e0e7a710f
Afnan Bin Abbas <afnanbabbas@gmail.com>
2024-06-06 20:26:13 +0500

    script

---
+++ script.sh
@@ -0,0 +1,17 @@
+if [ "$1" = "" ]; then
+
+  echo "You forgot to enter the IP address!"
+
+  echo "Syntax: ./pingsweep.sh your_ip_address"
+
+else
+
+  for ip in $(seq 1 254); do
+
+    ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+
+  done
+
+  echo "scan completed successfully!"
+
+fi
